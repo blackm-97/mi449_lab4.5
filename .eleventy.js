e0b5acb0b8853e358341960b39751d2b60093c81
blackm-97 <blackm97@msu.edu>
2025-02-07 18:32:54 +0000

    Wireframe done

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles");
 
   eleventyConfig.addShortcode('planet', async function(name, data) {
-    return `<section>
+    return `<section class="${name}">
+    <span class="dot"></span>
     <h2>${name}</h2>
     <p>${data}</p>
     </section>`
