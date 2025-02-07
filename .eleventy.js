cc281e3193070b8129e3777b8c69554ab91a6d5d
Connor Blackmer <blackm97@msu.edu>
2025-02-07 13:05:30 -0500

    Setup

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode('planet', async function(name, data) {
+    return `<section>
+    <h2>${name}</h2>
+    <p>${data}</p>
+    </section>`
+  });
 };
